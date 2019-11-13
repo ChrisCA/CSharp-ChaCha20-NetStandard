fa8f671cf0ff4b5650226e2f1cab0284e35fc226
Christian Albrecht <christianalbrechtca@gmail.com>
2019-11-13 18:58:55 +0100

    improved exceptions

--- CSChaCha20.cs
+++ CSChaCha20.cs
@@ -171,22 +171,22 @@ namespace CSChaCha20
         {
             if (input == null)
             {
-                throw new ArgumentNullException("input", "Input cannot be null");
+                throw new ArgumentNullException(nameof(input), "Input cannot be null");
             }
 
             if (output == null)
             {
-                throw new ArgumentNullException("output", "Output cannot be null");
+                throw new ArgumentNullException(nameof(output), "Output cannot be null");
             }
 
             if (numBytes < 0 || numBytes > input.Length)
             {
-                throw new ArgumentOutOfRangeException("numBytes", "The number of bytes to read must be between [0..input.Length]");
+                throw new ArgumentOutOfRangeException(nameof(numBytes), "The number of bytes to read must be between [0..input.Length]");
             }
 
             if (output.Length < numBytes)
             {
-                throw new ArgumentOutOfRangeException("output", $"Output byte array should be able to take at least {numBytes}");
+                throw new ArgumentOutOfRangeException(nameof(output), $"Output byte array should be able to take at least {numBytes}");
             }
 
             uint[] x = new uint[stateLength];    // Working buffer
